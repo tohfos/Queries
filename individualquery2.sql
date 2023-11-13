34e834f0c9c36cc387ed750f1af4a9e8d67a9643
tohfos <119699153+tohfos@users.noreply.github.com>
2023-11-13 22:05:44 +0200

    2done

--- individualquery2.sql
+++ individualquery2.sql
@@ -11,6 +11,8 @@
 select * from "Track" where "Composer" is not null;
 
 --create index idx_btree2 on "Track" USING Hash("Composer") 
+
+-- we realized that this query returns exactly 2525 rows out of 3503 which is 72% of the whole table , a index is useless in this case as a sequential scan would be much faster as it is costy to hop back and forth between the index and the actual table which is why our tps increased
 -- latency average = 1.244 ms
 -- tps = 803.733036 (including connections establishing)
 -- tps = 803.744891 (excluding connections establishing)
