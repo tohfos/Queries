17189e94c9d3265a96ac44ee6001d80ce1adad9b
Youssrakurayem <121040959+Youssrakurayem@users.noreply.github.com>
2023-11-14 19:09:00 +0200

    6 and 9

--- individualquery6.sql
+++ individualquery6.sql
@@ -7,13 +7,15 @@
 --total_exec_time =154197.62600499895
 
 
--- this query retrieves way too many records
+ 
 select * from "Track" where "UnitPrice" = 0.99;
--- latency average = 2.392 ms
--- tps = 418.004652 (including connections establishing)
--- tps = 418.013818 (excluding connections establishing)
+--create index UnitPrice2 on "Track"("UnitPrice") where ("UnitPrice")= 0.99
+--we created a partial index as there was a condition on the UnitPrice 
+--this query retrieves way too many records so creating an index on this query will not be efficient 
+--however after creating the index:
+--latency average = 2.537 ms
+--tps = 394.188162 (including connections establishing)
+--tps = 394.193902 (excluding connections establishing)
 
-
-
-
+--After analyzing the performance metrics before and after creating the index for the query, it's evident that the introduced index did not result in the expected improvements. In fact, the latency average increased, indicating a longer average response time, and the transactions per second (TPS) decreased, reflecting a decline in overall throughput.
 
