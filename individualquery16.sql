be6eb3d3182e7e1e5ccad2007ab62637b63add83
Youssrakurayem <121040959+Youssrakurayem@users.noreply.github.com>
2023-11-14 20:10:30 +0200

    16

--- individualquery16.sql
+++ individualquery16.sql
@@ -11,3 +11,10 @@ select "BillingCity", "Total" from "Invoice"
 group by 1, 2
 order by 2 Desc
 limit 1;
+
+--CREATE INDEX total ON "Invoice" USING BTREE ("Total");
+--we created a BTREE index as it's the best index in terms of sorting , and since we need the total to be ordered DESC so the BTREE index will be the best as it will be picked easily.
+
+--latency average = 0.182 ms
+--tps = 5507.072831 (including connections establishing)
+--tps = 5507.779643 (excluding connections establishing)
