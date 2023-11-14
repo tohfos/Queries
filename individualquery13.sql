bbbc602cea29d60e3a65dcbd76380cb84d35dca2
tohfos <119699153+tohfos@users.noreply.github.com>
2023-11-14 15:20:40 +0200

    13

--- individualquery13.sql
+++ individualquery13.sql
@@ -16,7 +16,7 @@ where "Country" = 'Brazil';
 
 
 --create index idx_Customer on "Customer" ("Country") where "Country" = 'Brazil'
--- As expected the index was not used as Custome table size is 59 and the query returns only 5 rows , meaning a sequential scan is more effiecient than a index, however in case the table is larger and the data is more diverse then this index would be optimal as it is created on the condition needed meaning it will get us a subset that has all the desired data.
+-- As expected the index was not used as Customer table size is 59 and the query returns only 5 rows , meaning a sequential scan is more effiecient than a index, however in case the table is larger and the data is more diverse then this index would be optimal as it is created on the condition needed meaning it will get us a subset that has all the desired data.
 
 
 --latency average = 0.129 ms
