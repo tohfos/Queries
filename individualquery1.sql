5b5c5f384e4620cb485f71fe6c46076303ba6254
Youssrakurayem <121040959+Youssrakurayem@users.noreply.github.com>
2023-11-15 22:06:38 +0200

    1

--- individualquery1.sql
+++ individualquery1.sql
@@ -1,6 +1,6 @@
 --Query 1
 
-
+select * from "Track" where "Composer" is null;
 
 
 -- latency average = 0.647 ms
@@ -11,14 +11,17 @@
 -- total_plan_time = 2905.8313840000146
 
 
--- created a partial/sparse index on album id as i have noticed that it would make more sense to have an index table that has all the album ids where composer is null as it would be smaller in size and just return all tracks related to all the albums that have a null composer. 
-select * from "Track" where "Composer" is null;
+--we decided to create an index for the "AlbumId" column. This index only includes records where the "Composer" is null.
+-- The idea is that this focused index will be smaller and quicker to search through. It's meant to speed up queries when we're looking for tracks related to albums with no specified composer. The goal is to make things more efficient by narrowing down the index to just the records that matter for these specific cases. 
+
 --CREATE INDEX partial_AlbumId ON "Track" ("AlbumId") WHERE "Composer" IS NULL;
 
 -- values after executing query:
+
 -- latency average = 0.566 ms
 -- tps = 1767.237871 (including connections establishing)
 -- tps = 1767.304355 (excluding connections establishing)
+
 -- idx_scan  = 100001
 -- idx_tup_read = 97800978
 -- idx_tup_fetch = 97800978
