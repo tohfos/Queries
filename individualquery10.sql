f58ec9aab2aec97f1fad379722d1d56f0c0060c4
tohfos <119699153+tohfos@users.noreply.github.com>
2023-11-15 22:20:15 +0200

    10 done

--- individualquery10.sql
+++ individualquery10.sql
@@ -10,3 +10,12 @@
 select ar."Name", a."Title" from "Playlist" p inner join "PlaylistTrack" pt on p."PlaylistId" = pt."PlaylistId"
 inner join "Track" t on t."TrackId" = pt."TrackId" inner join "Album" a on a."AlbumId" = t."AlbumId" inner join "Artist" ar on ar."ArtistId" = a."ArtistId" 
 Group by a."Title", ar."Name";
+
+
+-- as shown below we created a btree index on the artistID (Foreign Key) in the Album table which did really well in terms of usage with a good fetching rate , we used BTREE due to its balanced structure and its effieciency in terms of equality searching. 
+
+-- CREATE index idx_artist_id ON "Album" ("ArtistId");
+
+-- latency average = 5.483 ms
+-- tps = 182.382907 (including connections establishing)
+-- tps = 182.383647 (excluding connections establishing)
